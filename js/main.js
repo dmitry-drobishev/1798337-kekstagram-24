f735962b7f2156896d734860c3a72572983435ab
dmitry-drobishev <mitya.drobishev.77rus@yandex.ru>
2021-09-30 08:00:37 +0300

    добавил throw Error

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 // Учтите, что диапазон может быть только положительный, включая ноль.
 // А также придумайте, как функция должна вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему.
-
 function getRandomIntInclusive(min, max) {
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  if (min < 0 || max <= min) {
-    return false;
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  if (minValue >= 0 && maxValue > minValue) {
+    return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
   }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  throw new Error('Некорректное значение аргумента');
 }
 
-getRandomIntInclusive(-100, 1000.2);
+getRandomIntInclusive(-100, 101);
 
 // Функция для проверки максимальной длины строки.
-
 function stringLengthCheck(myString, maxLength) {
   return myString.length <= maxLength;
 }
