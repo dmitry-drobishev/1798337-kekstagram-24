220d5dcc90d72a2e7389480e00601691f35360b6
dmitry-drobishev <mitya.drobishev.77rus@yandex.ru>
2021-10-19 01:44:04 +0300

    добавил провеку форм

--- js/utils/upload-form.js
+++ js/utils/upload-form.js
@@ -2,6 +2,8 @@ const photoModal = document.querySelector('.img-upload__overlay');
 const siteBody = document.querySelector('body');
 const openModalButton = document.querySelector('#upload-file');
 const closeModalButton = document.querySelector('#upload-cancel');
+const userComment = document.querySelector('.text__description');
+
 const isEscKey = (evt) => evt.key === 'Escape';
 
 const onModalEscKeydown = (evt) => {
@@ -23,6 +25,7 @@ function closePhotoModal () {
   siteBody.classList.remove('.modal-open');
 
   document.addEventListener('keydown', onModalEscKeydown);
+  openModalButton.value = null;
 }
 
 openModalButton.addEventListener('change', () => {
@@ -32,3 +35,30 @@ openModalButton.addEventListener('change', () => {
 closeModalButton.addEventListener('click', () => {
   closePhotoModal ();
 });
+
+const reHashtags =  /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+
+const userHashtagInput = document.querySelector('.text__hashtags');
+
+userHashtagInput.addEventListener('input', () => {
+
+  const hashtagsArray = userHashtagInput.value.split(' ');
+  for (let hashtag = 0; hashtag < hashtagsArray.length; hashtag++) {
+    if (reHashtags.test(hashtagsArray[hashtag])) {
+      userHashtagInput.setCustomValidity('');
+    } else {
+      userHashtagInput.setCustomValidity('Херню ввёл');
+      break;
+    }
+  }
+  userHashtagInput.reportValidity();
+});
+
+userComment.addEventListener('keydown', (evt) => {
+  if (isEscKey(evt)) {
+    evt.stopPropagation();
+  }
+});
+
+// #upload-select-image
+//reset form
