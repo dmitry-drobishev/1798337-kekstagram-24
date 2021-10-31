163431a094dbfa2a89f115a1acd2c2c6129f4f7d
dmitry-drobishev <mitya.drobishev.77rus@yandex.ru>
2021-10-31 18:05:59 +0300

    добавил редактирование масштаба изображения

---
+++ js/logic/scale-picture.js
@@ -0,0 +1,33 @@
+const imgUploadPopup = document.querySelector('.img-upload'); // попап с загрузкой нового изображения
+const scaleUpButton = imgUploadPopup.querySelector('.scale__control--bigger');
+const scaleDownButton = imgUploadPopup.querySelector('.scale__control--smaller');
+const scaleControlCounter = imgUploadPopup.querySelector('.scale__control--value');
+const uploadPicture = imgUploadPopup.querySelector('.img-upload__preview img');
+const SCALE_STEP = 25;
+const MAX_SCALE = 100;
+const MIN_SCALE = 25;
+
+
+const scaleUploadPicture = () => {
+  scaleUpButton.addEventListener('click', () => {
+
+    let scaleNewValue = parseInt(scaleControlCounter.value, 10) + SCALE_STEP;
+    if (scaleNewValue > MAX_SCALE) {
+      scaleNewValue = MAX_SCALE;
+    }
+    scaleControlCounter.value = `${scaleNewValue}%`;
+    uploadPicture.style.transform = `scale(${scaleNewValue}%)`;
+  });
+
+  scaleDownButton.addEventListener('click', () => {
+
+    let scaleNewValue = parseInt(scaleControlCounter.value, 10) - SCALE_STEP;
+    if (scaleNewValue < MIN_SCALE) {
+      scaleNewValue = MIN_SCALE;
+    }
+    scaleControlCounter.value = `${scaleNewValue}%`;
+    uploadPicture.style.transform = `scale(${scaleNewValue}%)`;
+  });
+};
+
+export { scaleUploadPicture };
--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ import { createPosts } from './logic/create-posts.js';
 import { createMinPictures } from './logic/create-min-pictures.js';
 import { initPostsPreviews } from './logic/open-full-picture.js';
 import { initUploadForm } from './logic/upload-form.js';
+import { scaleUploadPicture } from './logic/scale-picture.js';
 
 initUploadForm();
 const postsArray = createPosts(20);
 createMinPictures(postsArray);
 initPostsPreviews(postsArray);
+scaleUploadPicture();
