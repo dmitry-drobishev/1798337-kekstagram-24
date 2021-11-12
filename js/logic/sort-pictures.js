e323d955f2a652ea09dd93c34a42d646551cc6a6
stooped-dog <mitya.drobishev.77rus@yandex.ru>
2021-11-12 17:28:17 +0300

    добавил сортировку постов

--- js/logic/sort-pictures.js
+++ js/logic/sort-pictures.js
@@ -1,19 +1,56 @@
+import { getRandomPositiveInteger } from '../utils/get-random-positive-integer.js';
+import { createMinPictures } from './create-min-pictures.js';
 const minPicturesFilter = document.querySelector('.img-filters');
-// let postsArray = [];
+let postsArray = [];
 
 const showFilters = () => {
   minPicturesFilter.classList.remove('img-filters--inactive');
 };
 
-// const setPostsArray = (array) => {
-//   postsArray = array;
-// };
+const setPostsArray = (array) => {
+  postsArray = array;
+};
 
-// const shuffle = (array) => {
-//   array.sort(() => Math.random() - 0.5);
-// };
+const shuffleArray = (array) => {
+  array.sort(() => Math.random() - 0.5);
+  const newArray = array.slice(0, 10);
+  return newArray;
+};
 
-// const getRandomArray = (array) => shuffle(array.slice());
+// Функция возвращает новый массив с 10 рандомными элементами передаваемого (функция отличная но не подходит)
+const getRandomArray = (array) => {
+  const setRandom = new Set();
+  while (setRandom.size < 10) {
+    const i = getRandomPositiveInteger(0, 24);
+    if (!setRandom.has(i)) {
+      setRandom.add(i);
+    }
+  }
+  const newRandomArray = [];
+  for (const i of setRandom) {
+    newRandomArray.push(array[i]);
+  }
+  return newRandomArray;
+};
+
+// Функция очищает страницу от миниатюр фотографий
+const removeMinPictures = () => {
+  document.querySelectorAll('.picture').forEach((element) => {
+    element.remove();
+  });
+};
+
+// Функция сортировки
+const comparePosts = (postA, postB) => {
+  const rankA = postA.comments.length;
+  const rankB = postB.comments.length;
+  return rankA - rankB;
+};
+
+const comparePostsArray = (array) => {
+  const newArray = array.slice().sort(comparePosts);
+  createMinPictures(newArray);
+};
 
 const sortMinPictures = () => {
   minPicturesFilter.addEventListener('click', (evt) => {
@@ -23,18 +60,24 @@ const sortMinPictures = () => {
       const filterName = evt.target.id;
       switch (filterName) {
         case 'filter-default':
+          removeMinPictures();
+          createMinPictures(postsArray);
           break;
         case 'filter-random':
+          removeMinPictures();
+          createMinPictures(shuffleArray(postsArray));
           break;
         case 'filter-discussed':
+          removeMinPictures();
+          comparePostsArray(postsArray);
           break;
       }
     }
+
   });
 };
 
-export { showFilters, sortMinPictures };
-// export { showFilters, sortMinPictures, setPostsArray };
+export { showFilters, sortMinPictures, getRandomArray, setPostsArray, shuffleArray};
 
 
 // 1) функции для сортировки 10 случайных. предаю массив объектов. ранодомлю копированный массив, вырезаю 10 элементов и передаю в функцию createMinPicture
@@ -42,4 +85,5 @@ export { showFilters, sortMinPictures };
 // document.querySelector('.pictures').innerHTML = '';
 
 // document.querySelectorAll('.picture').forEach((element) => {element.remove()});
+// const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,23,24];
 
--- js/main.js
+++ js/main.js
@@ -5,12 +5,15 @@ import { getData } from './logic/api.js';
 import { showAlert } from './logic/show-alert.js';
 import { showFilters, sortMinPictures } from './logic/sort-pictures.js';
 import { setPostsArray } from './logic/sort-pictures.js';
+// import { shuffleArray } from './logic/sort-pictures.js';
+// import { getRandomArray } from './logic/sort-pictures.js';
 
 const initContent = (postsArray) => {
   createMinPictures(postsArray);
   initPostsPreviews(postsArray);
   showFilters();
   setPostsArray(postsArray);
+  // createMinPictures(getRandomArray(postsArray));
 };
 
 initUploadForm();
