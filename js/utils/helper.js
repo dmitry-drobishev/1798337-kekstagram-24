2c143b03893644888271a8c25a28c744461ce218
dmitry-drobishev <mitya.drobishev.77rus@yandex.ru>
2021-11-13 16:33:28 +0300

    исправил функцию shuffleArray

--- js/utils/helper.js
+++ js/utils/helper.js
@@ -1,8 +1,5 @@
 const isEscKey = (evt) => evt.key === 'Escape';
 
-const shuffleArray = (array) => {
-  array.sort(() => Math.random() - 0.5);
-  return array;
-};
+const shuffleArray = (array) => array.slice().sort(() => Math.random() - 0.5);
 
 export { isEscKey, shuffleArray };
